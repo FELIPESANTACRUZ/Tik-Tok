425eac033e2b425d4100b5ce9daaf0fbcbc9676c
Felipe Santa Cruz Simão <felipesantacruz6@gmail.com>
2023-03-31 20:28:36 -0300

    feat: adicionado sidebar, criando funcoes de like, message e share e preparando para o banco de dados

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import Video from './pages/Video';
+import "./App.css";
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div className="App">
-      <div className='app__videos'>
-       <Video />
-       <Video />
-       <Video />
-        </div>
-      
+      <div className="app__videos">
+        <Video
+         likes={111}
+         messages={222}
+         shares={333} 
+         name="LIMINHA"
+         description="Gato top"
+         music="musica épica"
+         url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+         />
+
+        <Video
+         likes={444}
+         messages={555}
+         shares={777}
+         name="LIMINHA 2"
+         description="Gato top 2"
+         music="musica épica 2"
+         url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"        
+         />
+      </div>
     </div>
   );
 }
